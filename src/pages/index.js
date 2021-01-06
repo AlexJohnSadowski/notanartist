64809d74a8424ee98e2fc17a138016e223db43c7
sadovsky.aleksander@gmail.com <sadovsky.aleksander@gmail.com>
2021-01-06 01:21:19 +0100

    react helmet + icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import "../styles/styles.scss"
 import Header from "../components/Navbar/Header"
 
+import {Helmet} from "react-helmet";
+
 import LP from "../components/Mainpage/LP"
 import About from "../components/About/About"
 import ActOne from "../components/ACT I/ActOne"
@@ -10,22 +12,26 @@ import ActTwo from "../components/ACT II/ActTwo"
 import ActThree from "../components/ACT III/ActThree"
 import TheEnd from "../components/The End/TheEnd"
 
-
-
-
-
 const IndexPage = () => (
   <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Not An Artist</title>
+      <meta
+        name="description"
+        content="Definitely not an artistic website. I'm definitely not an artist, photographer, writer or whatsoever. Trust me."
+      />
+    </Helmet>
     <div className="lp-background">
       <Header />
       <LP />
     </div>
-    <About/>
-    <ActOne/>
-    <ActOneSecondPage/>
+    <About />
+    <ActOne />
+    <ActOneSecondPage />
     <ActTwo />
     <ActThree />
-    <TheEnd/>
+    <TheEnd />
   </>
 )
 
